cdbdd328a5e7cdc77363bbe16fa74c758e4442ce
Hastur <mgasca12@gmail.com>
2022-10-24 20:14:03 -0500

    feat: Ejercicio de Class Component

--- 5-Front_End/4-React/class-compontes/Hola-Mundo/src/App.jsx
+++ 5-Front_End/4-React/class-compontes/Hola-Mundo/src/App.jsx
@@ -1,36 +1,55 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import { Component } from 'react'
 
-function App() {
 
-  //functional component
-  const [count, setCount] = useState(0)
+//1. Class Component
+// Extender de Component
+class App extends Component {
+  
+  //2. Mandar llamar a render
 
-  return (
+  state = {
+    count: 0,
+    age: 8,
+    name: 'Fili',
+  };
+
+  render() {
+
+    const name = 'g18';
+    const day = 'Lunes';
+
+    const greetings = () => {
+      return  (
+        <div>
+          <p> Saludos desde mi función </p>
+        </div>
+      );
+    }
+
+    //3. Regresar JSX
+    return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h5>Holis :3</h5>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <span> Ésta es la generación {name}</span>
+        <h4>Hoy es el día: {day} </h4>
+        {greetings()}
+          {/* para setear estados se usa this.setState ({}) */}
+        <p>Valor de contador is {this.state.count}</p>
+        <button onClick={() => this.setState({count: this.state.count + 1})}>
+        
+        Incrementar
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={() => this.setState({count: this.state.count - 1})}>
+        
+        Decrementar
+        </button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  )}
 }
 
 export default App
