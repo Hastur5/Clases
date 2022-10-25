0ba8d3414a6b5cbffb6186c79bf93d4cca4dd42a
Hastur <mgasca12@gmail.com>
2022-10-24 21:33:48 -0500

    feat: ejercicio con styles

--- 5-Front_End/4-React/class-compontes/Hola-Mundo/src/App.jsx
+++ 5-Front_End/4-React/class-compontes/Hola-Mundo/src/App.jsx
@@ -12,8 +12,7 @@ class App extends Component {
 
   state = {
     count: 0,
-    age: 8,
-    name: 'Fili',
+    isDarkMode: false
   };
 
   render() {
@@ -22,21 +21,52 @@ class App extends Component {
     const day = 'Lunes';
 
     const greetings = () => {
+      if(this.state.count >=18){
       return  (
-        <div>
-          <p> Saludos desde mi función </p>
-        </div>
+          <p> Eres mayor de edad. </p>
       );
-    }
+      } else return <p>Estás Shikito</p>
+    };
+
+   const darkModeOff = {
+    fontSize: '35px', 
+    borderStyle: 'inset',
+    color: 'black',
+    backgroundColor: 'white'
+   };
+
+   const darkModeOn = {
+    fontSize: '35px', 
+    borderStyle: 'inset'
+   };
+
+
+
 
     //3. Regresar JSX
     return (
     <div className="App">
       <h5>Holis :3</h5>
-      <div className="card">
+
+    {/* Estilos: Se maneja através de objetos
+    Se usa camelCase. */}
+      <div className="card" 
+        style={this.state.isDarkMode ? darkModeOn: darkModeOff}>
         <span> Ésta es la generación {name}</span>
         <h4>Hoy es el día: {day} </h4>
-        {greetings()}
+        {/* Conditional Rendering IF */}
+        {/* 1er Escenario if () {} */}
+        {/* {this.state.count >= 18 && (
+        <p> Eres mayor de edad con Conditional Rendering.</p>
+        )} */}
+        {/* 2do Escenario if () {} else {} */}
+        {this.state.count >= 18 ?(
+          <p>Eres mayor de edad.</p>
+        ) : (
+          <p>Estás shikito, bro</p>
+        )
+        }
+        {/* {greetings()} */}
           {/* para setear estados se usa this.setState ({}) */}
         <p>Valor de contador is {this.state.count}</p>
         <button onClick={() => this.setState({count: this.state.count + 1})}>
@@ -44,12 +74,17 @@ class App extends Component {
         Incrementar
         </button>
         <button onClick={() => this.setState({count: this.state.count - 1})}>
-        
+
         Decrementar
+        </button>
+        <br />
+       
+        <button onClick={() => this.setState({isDarkMode: !this.state.isDarkMode})}>
+          Cambiar
         </button>
       </div>
     </div>
   )}
-}
+};
 
 export default App
