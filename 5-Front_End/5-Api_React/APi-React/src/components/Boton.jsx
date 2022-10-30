22b415e8019225114b77704fa0d02e83b918a9c1
Hastur5 <mgasca12@gmail.com>
2022-10-30 05:22:16 -0600

    feat: se sube select, falta corregir

--- 5-Front_End/5-Api_React/APi-React/src/components/Boton.jsx
+++
@@ -1,7 +0,0 @@
-agua = (event) => {
-    let resultados = this.state.pokemones.filter((pokemon) => {
-      return pokemon.name.toLowerCase().includes(q)
-
-    })
-    this.setState({resultados:resultados})
-  }
--- 5-Front_End/5-Api_React/APi-React/src/components/List.jsx
+++ 5-Front_End/5-Api_React/APi-React/src/components/List.jsx
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 import axios from 'axios';
 import Pokemon from "./Pokemon";
+import Select from 'react-select';
 
 class List extends Component {
 
@@ -9,10 +10,11 @@ class List extends Component {
         resultados: []
     }
 
-    constructor(props){
+    constructor(props) {
         super(props)
         //This biding is neccesary to make ´this´ work in the callback.
         this.buscar = this.buscar.bind(this)
+        // this.tipo = this.tipo.bind(this)
     }
 
     componentDidMount(){
@@ -34,21 +36,27 @@ class List extends Component {
       this.setState({resultados:filtrados})
     }
 
-    agua = (event) => {
+    tipo (event) {
+        let skip = event.currentTarget.value()
         let filtrados = this.state.pokemones.filter((pokemon) => {
-          return pokemon.type.includes("water")
+            return pokemon.type.includes(skip)
         })
         this.setState({resultados:filtrados})
-      }
-
+    }
+    
     render(){
         return(
             <div className="column">
                 <div className="navbar is-black is-fixed-top">
                     <img src="https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg" alt="Pokedex" width="150" height="50"/>
                     <input className="input is-rounded is-expanded" onKeyUp={this.buscar} type="text" placeholder="Buscar"></input>
-                    <br />
-                    <button onClick={this.agua} className='button'>Ver pokemones Agua</button>
+                    <br></br>
+                    <select onChange={this.tipo}>
+                   {this.state.pokemones.map(pokemones =>
+                        (<option key={pokemones.id} value={pokemones.id}>{pokemones.type}></option>
+                        )
+                    )}
+                   </select>
                 </div>
 
                 <div className='columns is-mobile is-multiline is-centered'>
