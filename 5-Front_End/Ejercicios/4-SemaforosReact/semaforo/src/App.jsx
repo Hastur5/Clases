28da2b9c26bdbb7a6b7b92cfe580e871106c0481
Hastur <mgasca12@gmail.com>
2022-10-31 12:37:13 -0600

    Subo actualizaciones del semáforo.

--- 5-Front_End/Ejercicios/4-SemaforosReact/semaforo/src/App.jsx
+++ 5-Front_End/Ejercicios/4-SemaforosReact/semaforo/src/App.jsx
@@ -2,6 +2,7 @@ import { Component } from 'react'
 import './App.css'
 import LuzVerde from './components/LuzVerde';
 import LuzAmarilla from './components/LuzAmarilla';
+import LuzRoja from './components/LuzRoja';
 
 class App extends Component {
 
@@ -17,9 +18,7 @@ state = {
 
         <h4>El semáforo está en: {this.state.luz}</h4>
         
-        {this.state.luz === 'verde' ? <LuzVerde /> : <LuzAmarilla/> }
-
-        {/* {this.state.light === "green" ? <GreenLight/>: ( this.state.light === "yellow" ? <YellowLight/>: <RedLight/> )} */}
+        {this.state.luz === 'verde' ? <LuzVerde /> :  this.state.luz === "amarilla" ? <LuzAmarilla/> : this.state.luz === "rojo" <LuzRoja/> }
 
 
         <button onClick = {() => {
