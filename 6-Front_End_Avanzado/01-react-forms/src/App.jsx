42744dd23f71323f0f141dd7a22676f4082df689
Hastur <mgasca12@gmail.com>
2022-11-22 21:40:54 -0600

    Se crea nuevo proyecto Fron End Avanzado

---
+++ 6-Front_End_Avanzado/01-react-forms/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import RefDom from '../components/RefDom'
+import './App.css'
+
+function App () {
+  const [,] = useState()
+
+  return (
+    <div className='App'>
+      <RefDom />
+    </div>
+  )
+}
+
+export default App
