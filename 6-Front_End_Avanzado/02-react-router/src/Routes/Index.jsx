c905b216d1b4173c6cfbb8b5d0227891b237c63b
Hastur <mgasca12@gmail.com>
2022-11-30 19:15:35 -0600

    Se agregan las rutas funcionando.

---
+++ 6-Front_End_Avanzado/02-react-router/src/Routes/Index.jsx
@@ -0,0 +1,17 @@
+// Importar React Router: Routes y Route.
+import { Routes, Route } from 'react-router-dom'
+import Contacto from '../pages/Contacto'
+import Home from '../pages/Home'
+import Portafolio from '../pages/Portafolio'
+
+const RoutesIndex = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/portafolio' element={<Portafolio />} />
+      <Route path='/contacto' element={<Contacto />} />
+    </Routes>
+  )
+}
+
+export default RoutesIndex
